fc21211a17c79329b719d2a1bf4378f16810e8eb
Noitidart <noitidart@gmail.com>
2017-08-19 17:57:37 -0700

    favicon touchup - skeleton point

--- src/App/Pages/index.js
+++ src/App/Pages/index.js
@@ -1,5 +1,4 @@
 import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 import BodyDashboard from './BodyDashboard'
